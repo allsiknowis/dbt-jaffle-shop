265a0356d95ee5fc0e45550c126137fd8d9c6e63
Steven Ellingson <stevendanellingson@gmail.com>
2025-10-16 13:52:33 +0000

    added compare_queries.sql to analyses folder

--- analyses/compare_queries.sql
+++ analyses/compare_queries.sql
@@ -1,9 +1,19 @@
-{% set old_etl_relation=ref('customer_orders') %} 
+/*
+This query lists all column names from both the old and new models
+so we can easily spot any differences in naming.
+*/
+select
+    column_name,
+    'in_customer_orders' as source_model
+from {{ ref('customer_orders').database }}.{{ ref('customer_orders').schema }}.INFORMATION_SCHEMA.COLUMNS
+where table_name = '{{ ref('customer_orders').identifier }}'
 
-{% set dbt_relation=ref('fct_customer_orders') %}  {{ 
+union all
 
-audit_helper.compare_relations(
-        a_relation=old_etl_relation,
-        b_relation=dbt_relation,
-        primary_key="order_id"
-    ) }}
+select
+    column_name,
+    'in_fct_customer_orders' as source_model
+from {{ ref('fct_customer_orders').database }}.{{ ref('fct_customer_orders').schema }}.INFORMATION_SCHEMA.COLUMNS
+where table_name = '{{ ref('fct_customer_orders').identifier }}'
+
+order by 1, 2
